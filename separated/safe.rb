76cf0139ef1b707465c3e99c2655cd0c1146bef7
Omar Shammas <omar.shammas@gmail.com>
2013-09-02 22:43:42 -0400

    Removing thread, remnants of an older test

--- separated/safe.rb
+++ separated/safe.rb
@@ -4,15 +4,9 @@ require 'rubygems'
 require 'headless'
 require 'selenium-webdriver'
 
-def safe
-	p "just entered"
-	headless = Headless.new display: 100, reuse: true, destroy_at_exit: false
-	driver = Selenium::WebDriver.for :firefox
-	driver.navigate.to "https://google.com"
-	p "retrieved url  - #{driver.title}"
-	driver.quit
-end
-
-p "launching thread"
-t1=Thread.new{ safe() } 
-t1.join
+p "just entered"
+headless = Headless.new display: 100, reuse: true, destroy_at_exit: false
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "https://google.com"
+p "retrieved url  - #{driver.title}"
+driver.quit
